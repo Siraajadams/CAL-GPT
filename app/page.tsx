51178692b3780dd0f5c50fb13e8b2424ab73b2b6
Siraajadams <siraaj.adams2003@gmail.com>
2026-05-04 02:36:31 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useMemo, useState } from "react";
 import { Camera, CalendarDays, ChevronRight, ClipboardList, Download, HeartPulse, Home, LineChart, Lock, Mail, MapPin, Phone, Plus, Salad, ShieldCheck, Sparkles, User, Utensils, Weight } from "lucide-react";
 import { Button } from "@/components/ui/button";
