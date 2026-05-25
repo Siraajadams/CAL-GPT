96f627346f655d5ab9b6218e85cd8b63de0a1d74
Siraajadams <siraaj.adams2003@gmail.com>
2026-05-25 23:47:14 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,9 @@ const config: Config = {
       }
     }
   },
-  plugins: [require("@tailwindcss/forms")]
+plugins: [ require("@tailwindcss/forms"),
+  require("@tailwindcss/typography"),
+],
 };
 
 export default config;
