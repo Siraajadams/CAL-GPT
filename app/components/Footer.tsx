0a196f5f71554ea46f4c7e112fb5aeee10966098
Siraajadams <siraaj.adams2003@gmail.com>
2026-05-26 00:01:27 +0200

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-<div className="flex gap-4 text-sm">
-  <Link href="/privacy-policy">
-    Privacy Policy
-  </Link>
+    <div className="flex gap-4 text-sm">
+      <Link href="/privacy-policy">
+        Privacy Policy
+      </Link>
 
-  <Link href="/terms-and-conditions">
-    Terms & Conditions
-  </Link>
-</div>
+      <Link href="/terms-and-conditions">
+        Terms & Conditions
+      </Link>
+    </div>
+  );
+}
